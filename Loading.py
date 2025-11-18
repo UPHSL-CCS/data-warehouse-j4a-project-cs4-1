bda376dcb1097c96b99b84436a93bca3f54c63fc
Fraxinity <kelvinadam2004@gmail.com>
2025-11-18 14:56:04 +0800

    fixed datatype of column, due to new version of postgresql

--- Loading.py
+++ Loading.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-db_url = f'postgresql+psycopg2://postgres:SlungusMan@localhost:5433/Hospital'
+db_url = f'postgresql+psycopg2://postgres:1234@localhost:5432/Hospitales'
 csv_file = {
     'DimDate': 'transformed_data/DimDate.csv',  
     'DimDepartment': 'transformed_data/DimDepartment.csv',
@@ -27,11 +27,17 @@ def load_data_to_db():
 
         if table_name == 'DimDate':
             df['FullDate'] = pd.to_datetime(df['FullDate'])
+            df['IsWeekend'] = df['IsWeekend'].astype(bool) 
+
+        if table_name == 'DimDoctor':
+            df['PhoneNumber'] = df['PhoneNumber'].astype(str)
+
         if table_name == 'DimPatient':
             df['DateOfBirth'] = pd.to_datetime(df['DateOfBirth'])
             df['RegistrationDate'] = pd.to_datetime(df['RegistrationDate'])
+            df['ContactNumber'] = df['ContactNumber'].astype(str)
 
-        df.to_sql(table_name, engine, if_exists='replace', index=False, chunksize=1000)
+        df.to_sql(table_name, engine, if_exists='append', index=False, chunksize=1000)
 
 if __name__ == '__main__':
     load_data_to_db()
